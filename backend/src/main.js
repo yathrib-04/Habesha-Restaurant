112f8782cf64f2c9162a804c9bb51a89e01a2af4
yeabsiraAbiy <abiyyeabsra@gmail.com>
2025-01-12 09:10:01 +0300

    add the main js and ts

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,28 +1,27 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const core_1 = require("@nestjs/core");
-const app_module_1 = require("./app.module");
-const data_source_1 = require("./data-source"); // Import AppDataSource
+const core = require("@nestjs/core");
+const appModule = require("./app.module");
+const dataSource = require("./data-source"); // Import AppDataSource
+
 function bootstrap() {
     return __awaiter(this, void 0, void 0, function* () {
-        var _a;
-        // Initialize TypeORM
-        yield data_source_1.AppDataSource.initialize()
-            .then(() => console.log('Data Source has been initialized!'))
-            .catch((err) => console.error('Error during Data Source initialization', err));
-        const app = yield core_1.NestFactory.create(app_module_1.AppModule);
-        // Enable CORS
-        app.enableCors();
-        yield app.listen((_a = process.env.PORT) !== null && _a !== void 0 ? _a : 3000);
+        try {
+            // Initialize TypeORM
+            yield dataSource.AppDataSource.initialize();
+            console.log("Data Source has been initialized!");
+
+            const app = yield core.NestFactory.create(appModule.AppModule);
+
+            // Enable CORS
+            app.enableCors();
+
+            const port = process.env.PORT || 3000;
+            yield app.listen(port);
+            console.log(`Application is running on: ${yield app.getUrl()}`);
+        } catch (err) {
+            console.error("Error during Data Source initialization", err);
+        }
     });
 }
+
 bootstrap();
